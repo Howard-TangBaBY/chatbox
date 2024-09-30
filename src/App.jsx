9d136f89864cfdb41e1c0837e2426dc02792d32d
Howard-Tang <1024.howard@gmail.com>
2024-09-30 15:07:31 +0800

    chatList upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Chat from "./components/chat/Chat"
+import List from "./components/List/List"
+import Detail from "./components/details/Detail"
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className=' container '>
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
   )
 }
 
---
+++ src/components/List/chatList/ChatList.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react"
+import "./chatList.css"
+
+const ChatList = () => {
+  const [addMode, setAddMode] = useState(false)
+  return (
+    <div className='chatList'>
+        <div className="search">
+            <div className="searchbar">
+            <img src="./search.png" alt="" />
+            <input type="text" placeholder="Search"/>
+            </div>
+            <img src={addMode ? "./minus.png" : "./plus.png"}
+            alt="" 
+            className="add" 
+            onClick={() => setAddMode((prev) => ! prev)}/>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Zoka</span>
+                <p>Fuck u TangBaby!</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatList
