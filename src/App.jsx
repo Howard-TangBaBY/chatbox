c7583d4babbf4ed1ad9aabd4c5a4f4f5092c7495
Howard-Tang <1024.howard@gmail.com>
2024-10-07 01:46:51 +0800

    sending message update

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,46 @@
-import Chat from "./components/chat/Chat"
-import List from "./components/List/List"
-import Detail from "./components/details/Detail"
-import Login from "./components/login/Login"
-import Notification from "./components/notification/Notification"
-import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "./lib/firebase"
-import { useEffect } from "react"
-import { useUserStore } from "./lib/userStore"
-
+import Chat from "./components/chat/Chat";
+import List from "./components/List/List";
+import Detail from "./components/details/Detail";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useEffect } from "react";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
-  
-  const {currentUser, isLoading, fetchUserInfo} = useUserStore()
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
 
   useEffect(() => {
-    const unSub = onAuthStateChanged(auth, (user)=>{
-      fetchUserInfo(user?.uid)
-    })
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
 
     return () => {
       unSub();
-    }
-  }, [fetchUserInfo])
+    };
+  }, [fetchUserInfo]);
 
-  console.log(currentUser)
+  console.log(currentUser);
 
-
-  if(isLoading) return <div className="loading">Loading...</div>
+  if (isLoading) return <div className="loading">Loading...</div>;
 
   return (
-    <div className=' container '>
+    <div className=" container ">
       {currentUser ? (
         <>
-          <List/>
-          <Chat/>
-          <Detail/>
+          <List />
+          {chatId && <Chat />}
+          {chatId && <Detail />}
         </>
       ) : (
-        <Login/>
+        <Login />
       )}
-      <Notification/>
+      <Notification />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/List/chatList/ChatList.jsx
+++ src/components/List/chatList/ChatList.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from "react";
 import "./chatList.css";
 import AddUser from "./addUser/addUser";
 import { useUserStore } from "../../../lib/userStore";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { database } from "../../../lib/firebase";
+import { useChatStore } from "../../../lib/chatStore";
 
 const ChatList = () => {
   const [chats, setChats] = useState([]);
   const [addMode, setAddMode] = useState(false);
 
   const { currentUser } = useUserStore();
+  const { changeChat, chatId } = useChatStore();
 
   useEffect(() => {
     const unSub = onSnapshot(
@@ -37,7 +39,28 @@ const ChatList = () => {
     };
   }, [currentUser.id]);
 
-  console.log(chats);
+  const handleSelect = async (chat) => {
+    const userChats = chats.map((item) => {
+      const { user, ...rest } = item;
+      return rest;
+    });
+
+    const chatIndex = userChats.findIndex(
+      (item) => item.chatId === chat.chatId
+    );
+
+    userChats[chatIndex].isSeen = true;
+    const userChatsRef = doc(database, "userchats", currentUser.id);
+
+    try {
+      await updateDoc(userChatsRef, {
+        chats: userChats,
+      });
+      changeChat(chat.chatId, chat.user);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="chatList">
@@ -54,7 +77,14 @@ const ChatList = () => {
         />
       </div>
       {chats.map((chat) => (
-        <div className="item" key={chat.chatid}>
+        <div
+          className="item"
+          key={chat.chatid}
+          onClick={() => handleSelect(chat)}
+          style={{
+            backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
+          }}
+        >
           <img src={chat.user.avatar || "./avatar.png"} alt="" />
           <div className="texts">
             <span>{chat.user.username}</span>
